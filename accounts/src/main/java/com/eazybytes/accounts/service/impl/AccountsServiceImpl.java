9a363f65bc6924d0f4f075fe60185707c7e9610a
dasencioWork <dasencio@minsait.com>
2024-03-09 18:02:57 +0100

    section_2.0.2

---
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AccountsServiceImpl implements IAccountsService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+
+    }
+
+    @Override
+    public CustomerDto fetchAccount(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateAccount(CustomerDto customerDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAccount(String mobileNumber) {
+        return false;
+    }
+}
